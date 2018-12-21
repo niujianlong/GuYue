414468a77e1981c36b897f5a77a8d373e6e127bb
njl <286395446@qq.com>
2018-12-21 22:18:41 +0800

    初始版本

---
+++ Classes/game_ui/GameOptions.cpp
@@ -0,0 +1,26 @@
+
+
+#include "GameOptions.h"
+#include "GameScene.h"
+
+void GameOptions::onEnterTransitionDidFinish()
+{
+    CCNode::onEnterTransitionDidFinish();
+    
+    m_pBg = Sprite::create("ui/options.png");
+    m_pBg->setPosition(Vec2::ZERO);
+    this->addChild(m_pBg, -1);
+    
+    ControlButton* button = ControlButton::create(Scale9Sprite::create("ui/closed_normal.png"));
+    button->setBackgroundSpriteForState(Scale9Sprite::create("ui/closed_selected.png"), Control::State::HIGH_LIGHTED);
+    button->setPreferredSize(Size(57, 58));
+    button->setPosition(ccpSub(ccpAdd(m_pBg->getPosition(), m_pBg->getContentSize()), button->getContentSize()/2));
+    m_pBg->addChild(button);
+    
+    button->addTargetWithActionForControlEvents(GAME_UILAYER,
+                                                cccontrol_selector(GameInfoUIController::removeBigMenuAndButton),
+                                                Control::EventType::TOUCH_UP_INSIDE );
+    
+    
+  //  button->setTouchPriority(0);
+}
