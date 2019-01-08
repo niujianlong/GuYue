f3f525f24c4d6ff657addf1ecc1b3216c6a1f05b
njl <286395446@qq.com>
2019-01-08 19:13:08 +0800

    格式化了代码

--- Classes/back_pack/NodeBagItem.h
+++ Classes/back_pack/NodeBagItem.h
@@ -11,32 +11,32 @@ using namespace ui;
 using namespace cocostudio;
 
 //背包中的物品item（长方形），包含NodeBagGrid以及一些额外信息(名字，是否已装备等)
-class NodeBagItem : public Node{
+class NodeBagItem : public Node {
 public:
-    CREATE_FUNC(NodeBagItem);
-    virtual bool init();
+	CREATE_FUNC(NodeBagItem);
+	virtual bool init();
 
-    void setupNodeBagItem(NodeBagItemModel model);
+	void setupNodeBagItem(NodeBagItemModel model);
 
-    void setSelected(bool isSelected);
-    bool isSelected(){ return isSelected_; }
+	void setSelected(bool isSelected);
+	bool isSelected() { return isSelected_; }
 
-    ImageView* getBtn(){ return btn; }
+	ImageView* getBtn() { return btn; }
 
-    int64_t get_sno();
-    int get_id();
+	int64_t get_sno();
+	int get_id();
 
 private:
-    ImageView* btn;
-    NodeBagGrid* grid;
-    Text* name_label;
-    bool isSelected_;
-    Sprite* isSelected_sp;
+	ImageView* btn;
+	NodeBagGrid* grid;
+	Text* name_label;
+	bool isSelected_;
+	Sprite* isSelected_sp;
 
-    Sprite* sp_debris;
-    Sprite* already_use;
+	Sprite* sp_debris;
+	Sprite* already_use;
 private:
-    NodeBagItemModel view_model;
+	NodeBagItemModel view_model;
 };
 
 #endif // NodeBagItem_h__
